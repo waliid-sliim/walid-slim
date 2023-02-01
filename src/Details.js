d189edc00a2f79f8af5df1e9ce483eb09a59aefd
waliid-sliim <walidovicc@gmail.com>
2023-02-01 17:20:49 +0100

    intial commit

---
+++ src/Details.js
@@ -0,0 +1,179 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo11.png";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/walid4.png";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/native.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import figma from "./assets/techstack/figma.png";
+import next from "./assets/techstack/nextjs.png";
+import typescript from "./assets/techstack/typescript.png";
+import mongodb from "./assets/techstack/mongodb.png";
+import mysql from "./assets/techstack/mysql.png";
+import firebase from "./assets/techstack/firebase.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project3.png";
+import projectImage2 from "./assets/projects/project2.png";
+import projectImage3 from "./assets/projects/project1.png";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Walid Slim",
+  tagline: "I am a Fullstack JavaScript web developer",
+  img: profile,
+  about: `I am a freshly graduate Full Stack JS developer with experience building websites and web applications. 
+  I specialize in JavaScript , TypeScript and have experience working with React .
+   I also have experience working with NextJs, and React Native . 
+  could you please take a look at my work in github
+  
+  I'm open to work and implement my technical skills.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/slim-walid/",
+  github: "https://github.com/waliid-sliim",
+  twitter: "https://twitter.com/",
+  instagram: "https://www.instagram.com/slim__walid/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "ALL-HAIR",
+    Company: `SCRUM MASTER & FS DEVELOPER`,
+    Location: "RBK",
+    Type: "Full Time",
+    Duration: "4 Weeks",
+  },
+  {
+    Position: "CITY-SUITS",
+    Company: `SCRUM MASTER & FS DEVELOPER`,
+    Location: "RBK",
+    Type: "Full Time",
+    Duration: "2 Week",
+  },
+  {
+    Position: "GREEN-FEILD-SHOP",
+    Company: `FS DEVELOPER`,
+    Location: "RBK",
+    Type: "Full Time",
+    Duration: "1 Week",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Full Stack Web Developer",
+    Company: "RBK(RebootKamp)",
+    Location: "Tunis",
+    Type: "Full Time",
+    Duration: "Sep 2022 -Jan2023",
+  },
+  {
+    Position: "BTS industrial automation IT",
+    Company: `CSFMI `,
+    Location: "monastir",
+    Type: "Full Time",
+    Duration: "Sep 2019 - juin 2021",
+  },
+  {
+    Position: "The Higher Institute of Computer Science and Mathematics",
+    Company: `ISIM `,
+    Location: "monastir",
+    Type: "Full Time",
+    Duration: "Sep 2017 - juin 2018",
+  },
+  {
+    Position: "Mathematics bachelor",
+    Company: `Lycee Jammel`,
+    Location: "monastir",
+    Type: "Full Time",
+    Duration: "2016",
+  }
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+  next:next,
+  typescript:typescript,
+  mongodb:mongodb,
+  mysql:mysql,
+  firebase:firebase,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "ALL-HAIR",
+    image: projectImage1,
+    description: `A mobile application for beauty centers and barber
+    shops to help them to: - share their works & prices. -
+    Facilitate user interaction to book appointments online.`,
+    techstack: "HTML, JavaScript, React Native, Mysql, sequelize, Native Base, Nodejs, Express, Firebase,",
+    previewLink: "https://res.cloudinary.com/drd0uckic/video/upload/v1675265404/hdn87ymawcq2dxjmvclj.mp4",
+    githubLink: "https://github.com/waliid-sliim",
+  },
+  {
+    title: "CITY-SUITS",
+    image: projectImage2,
+    description: `An e commerce website that sells suits made from
+    high quality dedicated to men.
+    Through City-Suits website, a business can process orders, accept payments, manage shipping
+    `,
+    techstack: "HTML, TypeScript, Nextjs, Mongodb, boostrap, Javascript, CSS, Nodejs, Express,  jwt",
+    previewLink: "https://res.cloudinary.com/drd0uckic/video/upload/v1675265313/pfphho9sbbtubrltcxro.mp4",
+    githubLink: "https://github.com/waliid-sliim",
+  },
+  {
+    title: "GREEN-FEILD-SHOP",
+    image: projectImage3,
+    description: `A Customer  to Customer project aiming to create a
+    platform for users to buy and sell all kind of products
+    online .
+    Through G-F-S website, a business can process orders, accept payments, manage shipping.
+    `,
+    techstack: "HTML/CSS, JavaScript, Reactjs, mongodb, nodejs, expressjs, jwt",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com/waliid-sliim",
+  },
+ 
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "slim00walid@gmail.com",
+  phone: "+216 99 443 555",
+};
